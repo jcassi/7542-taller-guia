2d31f50bb03955aa95823abae543c5566d7b5aee
jcassi <julcassi@gmail.com>
2021-09-20 13:10:58 -0300

    Split program into functions

--- guia1/ejercicio2/src/main.rs
+++ guia1/ejercicio2/src/main.rs
@@ -3,34 +3,42 @@ use std::io::{self, prelude::*, BufReader};
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
-
-//Refactorizar separando en funciones luego de ver ownership y lifetimes
 fn main() -> io::Result<()> {
     let file = File::open("contar_palabras.txt")?;
     let reader = BufReader::new(file);
+    
+    let frequencies = process_lines(reader);
+    let mut frequencies = Vec::from_iter(frequencies.iter());
+    frequencies.sort_by(|a, b| (b.1).cmp(&(a.1)));
+
+    print_frequencies(frequencies);
+
+    Ok(())
+}
+
+fn process_lines(reader: BufReader<File>) -> HashMap<String, u32>{
     let mut frequencies: HashMap<String, u32> = HashMap::new();
 
     for line in reader.lines() {
         match line {
-            Ok(line) => {
-                let line = line.to_lowercase();
-                let words : Vec<&str> = line.split(" ").collect();
-                for word in words {
-                    let stat = frequencies.entry(word.to_string()).or_insert(0);
-                    *stat += 1;
-                }
-            }
+            Ok(line) => process_line(line, &mut frequencies),
             Err(_) => (),
         }
     }
+    frequencies
+}
 
-    let mut frequencies = Vec::from_iter(frequencies.iter());
-
-    frequencies.sort_by(|a, b| (b.1).cmp(&(a.1)));
-
+fn print_frequencies(frequencies: Vec<(&String, &u32)>) {
     for element in frequencies.iter() {
         println!("{} -> {}", element.0, element.1);
     }
+}
 
-    Ok(())
+fn process_line(linee: String, frequencies: &mut HashMap<String, u32>) {
+    let line = linee.to_lowercase();
+    let words : Vec<&str> = line.split(" ").collect();
+    for word in words {
+        let stat = frequencies.entry(word.to_string()).or_insert(0);
+        *stat += 1;
+    }
 }
